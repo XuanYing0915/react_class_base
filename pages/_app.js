dd3e90fd787309d52d6c213afc34df6b2bdbc25c
XuanYing0915 <xuanyinglife@gmail.com>
2023-07-13 14:04:38 +0800

    收藏功能(最終改為純涵式)

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 // @ 根目錄的意思
 
 import '@/styles/product-table.module.css'
+import '@/styles/menu.css'
+import '@/styles/book-list.css'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
