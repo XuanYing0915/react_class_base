5688a45ae0a58c1f9109acec5fabebd452f89b30
XuanYing0915 <xuanyinglife@gmail.com>
2023-07-14 11:22:51 +0800

    子元件

--- components/prop_demo2/child-a.js
+++ components/prop_demo2/child-a.js
@@ -2,7 +2,7 @@ export default function ChildA(props) {
   return (
     <>
       <h1>ChildA</h1>
-      <p>來自Parent(父母元件)的資料: {props.pData}</p>
+      <p>來自Parent(父元件)的資料: {props.pData}</p>
     </>
   )
 }
