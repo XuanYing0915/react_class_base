75596a59ca9b82889da6c89bcf958f73b6e0b711
XuanYing0915 <xuanyinglife@gmail.com>
2023-07-18 10:20:52 +0800

    新增+刪除純粹函式

--- pages/course/todo.js
+++ pages/course/todo.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import TodoIndex from '@/components/todo'
+import TodoIndex from '@/components/todo/index'
+import TodoIndexPlus from '@/components/todo/todo_plus'
 
 export default function Todo() {
   return (
     <>
       <h1>Todo待辨事項</h1>
       <TodoIndex />
+      <h1>新增純粹函式</h1>
+      <TodoIndexPlus />
     </>
   )
 }
