c67de2f391e8163a4e8e8cbff8ce55e56d437600
XuanYing0915 <xuanyinglife@gmail.com>
2023-07-18 09:32:55 +0800

    完成打勾

--- components/todo/index.js
+++ components/todo/index.js
@@ -8,10 +8,19 @@ export default function TodoIndex() {
   const [isCompositing, setIsCompositing] = useState(false)
 
   const [todos, setTodos] = useState([
-    { id: 1, text: '測試' },
-    { id: 2, text: '測試2' },
+    { id: 1, text: '測試', completed: false },
+    { id: 2, text: '測試2', completed: true },
     { id: 3, text: '測試3' },
   ])
+
+  const toggleCompleted = (id) => {
+    const newTodos = todos.map((v) => {
+      if (v.id === id) return { ...v, completed: !v.completed }
+      else return { ...v }
+    })
+
+    setTodos(newTodos)
+  }
   return (
     <>
       <input
@@ -30,9 +39,13 @@ export default function TodoIndex() {
           // 有值時(不是空白才能加入)
           if (e.key === 'Enter' && !isCompositing && inputText) {
             // 按按鈕  新增一個物件
-
+            //定義一個新的物件
             // id使用時間日期轉成毫秒的方式當id
-            const newTodo = { id: Number(new Date()), text: inputText }
+            const newTodo = {
+              id: Number(new Date()),
+              text: inputText,
+              completed: false,
+            }
             // 展開陣列 加入物件
             setTodos([newTodo, ...todos])
             // 清空輸入欄位
@@ -44,7 +57,20 @@ export default function TodoIndex() {
       <ul>
         {/* 將陣列物件渲染 */}
         {todos.map((v) => {
-          return <li key={v.id}>{v.text}</li>
+          return (
+            <li key={v.id}>
+              {/* checked true = 打勾  */}
+              <input
+                type="checkbox"
+                checked={v.completed}
+                onChange={() => {
+                  toggleCompleted(v.id)
+                }}
+              />
+              {/* 判斷  ture=上刪除線 */}
+              {v.completed ? <del>{v.text}</del> : v.text}
+            </li>
+          )
         })}
       </ul>
     </>
