41ec052c85d09073a352e3b8246e0deffb013547
XuanYing0915 <xuanyinglife@gmail.com>
2023-07-19 16:30:53 +0800

    動態路由

---
+++ pages/course/user/[name].js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+export default function UserName() {
+  const router = useRouter()
+
+  const { name } = router.query
+  console.log(router.query)
+  console.log(router.query)
+  return (
+    <>
+      <div>檔名可以是[].js 組成</div>
+      <div>name={name}</div>
+      {/* <div>name={router.query[`user-id`]}</div>
+      <div>name={router.query.uid}</div> */}
+    </>
+  )
+}
